f120caebfd9e8775033932b1c98ed8e0aeaf392e
Abdiel <contato@seorocket.com.br>
2025-10-21 23:14:12 -0300

    Adicionando dotenv para leitura de variáveis de ambiente42

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'; // ✅ Importação correta
+import '../public/assets/css/fonts-override.css'; // ✅ Importa fontes com swap
 import React, {useEffect} from 'react';
 import { animationCreate } from "../../utils/utils";
 import ScrollToTop from "react-scroll-to-top";
@@ -15,8 +16,14 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
           <Head>
-                
-            </Head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="Portfólio de Abdiel - Desenvolvedor Web" />
+            <link rel="preload" href="/assets/fonts/fa-solid-900.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+            <link rel="preload" href="/assets/fonts/fa-regular-400.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+            <link rel="preload" href="/assets/fonts/fa-light-300.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+            <link rel="preload" href="/assets/fonts/fa-brands-400.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          </Head>
             <div className="page-wrapper">
               <Component {...pageProps} />
             </div>
