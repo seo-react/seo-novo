836533f85b860c15f48d483f26b01643ee2d581f
Abdiel <contato@seorocket.com.br>
2025-11-04 16:37:51 -0300

    Adicionando dotenv para leitura de variáveis de ambiente47

--- src/components/case-study/CaseStudyTwo.js
+++ src/components/case-study/CaseStudyTwo.js
@@ -1,70 +1,45 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import { CaseStudyTwoData } from '@/data/case-study';
 import Link from 'next/link';
 
 const CaseStudyOne = () => {
-    useEffect(() => {
-
-        // Testimonial Two
-        if ($(".case-study-two__active").length) {
-            $('.case-study-two__active').owlCarousel({
-                loop: true,
-                navText: ['<i class="fa-light fa-arrow-left"></i>', '<i class="fa-light fa-arrow-right"></i>'],
-                nav: true,
-                autoHeight: true,
-                responsive: {
-                    0: {
-                        items: 1,
-                        stagePadding: 0,
-                    },
-                    576: {
-                        items: 1,
-                        stagePadding: 0,
-                    },
-                    768: {
-                        items: 1,
-                        stagePadding: 120,
-                    },
-                    992: {
-                        items: 1,
-                        stagePadding: 160,
-                    },
-                    1200: {
-                        items: 1,
-                        stagePadding: 150,
-                    },
-                    1400: {
-                        items: 1,
-                        stagePadding: 200,
-                    },
-                    1600: {
-                        items: 1,
-                        stagePadding: 290,
-                    },
-                }
-            });
-        }
-      
-    }, []);
-    return (
-        <>
-            {/* case study two start */}
-            <div className="case-study-two theme-gray-2 pb-140">
-                <div className="case-study-two__active owl-carousel">
-                    {CaseStudyTwoData.map((item, i) => (
-                        <div key={i} className="case-study-two__single" data-merge={item.no} data-background={item.image}>
-                            <div className="case-study-two__single-box">
-                                <span>{item.subheading}</span>
-                                <h4>{item.heading}</h4>
-                                <Link href={item.link}>{item.link_text}</Link>
-                            </div>
-                        </div>
-                    ))}
+  return (
+    <>
+      {/* case study two start */}
+      <div className="case-study-two theme-gray-2 pb-140">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          navigation
+          pagination={{ clickable: true }}
+          spaceBetween={30}
+          slidesPerView={1}
+          className="case-study-two__active"
+        >
+          {CaseStudyTwoData.map((item, i) => (
+            <SwiperSlide key={i}>
+              <div
+                className="case-study-two__single"
+                style={{ backgroundImage: `url(${item.image})` }}
+              >
+                <div className="case-study-two__single-box">
+                  <span>{item.subheading}</span>
+                  <h4>{item.heading}</h4>
+                  <Link href={item.link}>{item.link_text}</Link>
                 </div>
-            </div>
-            {/* case study two end */}
-        </>
-    )
-}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      {/* case study two end */}
+    </>
+  );
+};
 
 export default CaseStudyOne;
