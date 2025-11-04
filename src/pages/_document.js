c699df3a9498e540c96f3c0356e63edd954809d8
Abdiel <contato@seorocket.com.br>
2025-11-04 17:47:30 -0300

    Adicionando dotenv para leitura de variáveis de ambiente49

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,66 +1,127 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head>
-  {/* Otimização de fonts */}
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Otimização de fonts */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
 
-  {/* Favicons */}
-  <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/logo/favicon.png" />
-  <link rel="manifest" href="/assets/images/favicons/site.webmanifest" />
+        {/* Favicons */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/img/logo/favicon.png"
+        />
+        <link
+          rel="manifest"
+          href="/assets/images/favicons/site.webmanifest"
+        />
 
-  {/* Estilos externos otimizados */}
-  <link rel="preload" href="/assets/css/style.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/animate.min.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/bootstrap.min.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/fontawesome-all.min.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/magnific-popup.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/nice-select.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/owl.carousel.min.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/slick.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/swiper-bundle.min.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/seoq-core.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/seoq-unit.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/seoq-updater.css" as="style" onLoad="this.onload=null;this.rel='stylesheet'" />
+        {/* Estilos críticos (carregamento imediato) */}
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/assets/css/seoq-core.css" />
 
-  {/* Fallback para navegadores sem JS */}
-  <noscript>
-    <link rel="stylesheet" href="/assets/css/style.css" />
-    <link rel="stylesheet" href="/assets/css/animate.min.css" />
-    <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/assets/css/fontawesome-all.min.css" />
-    <link rel="stylesheet" href="/assets/css/magnific-popup.css" />
-    <link rel="stylesheet" href="/assets/css/nice-select.css" />
-    <link rel="stylesheet" href="/assets/css/owl.carousel.min.css" />
-    <link rel="stylesheet" href="/assets/css/slick.css" />
-    <link rel="stylesheet" href="/assets/css/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="/assets/css/seoq-core.css" />
-    <link rel="stylesheet" href="/assets/css/seoq-unit.css" />
-    <link rel="stylesheet" href="/assets/css/seoq-updater.css" />
-  </noscript>
-</Head>
-            <body>
-                <Main />
-                <NextScript />
-                <script src="/assets/js/jquery-3.6.0.min.js"></script>
-<script src="/assets/js/bootstrap.bundle.min.js"></script>
-<script src="/assets/js/imagesloaded.pkgd.min.js"></script>
-<script src="/assets/js/isotope.pkgd.min.js"></script>
-<script src="/assets/js/jquery.appear.min.js"></script>
-<script src="/assets/js/jquery.countup.min.js"></script>
-<script src="/assets/js/jquery.magnific-popup.min.js"></script>
-<script src="/assets/js/jquery.waypoints.min.js"></script>
-<script src="/assets/js/nice-select.js"></script>
-<script src="/assets/js/owl.carousel.min.js"></script>
-<script src="/assets/js/slick.min.js"></script>
-<script src="/assets/js/swiper-bundle.min.js"></script>
-<script src="/assets/js/wow.min.js"></script>
+        {/* Estilos não críticos (adiados) */}
+        <link
+          rel="stylesheet"
+          href="/assets/css/style.css"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <link
+          rel="stylesheet"
+          href="/assets/css/animate.min.css"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <link
+          rel="stylesheet"
+          href="/assets/css/fontawesome-all.min.css"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <link
+          rel="stylesheet"
+          href="/assets/css/magnific-popup.css"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <link
+          rel="stylesheet"
+          href="/assets/css/nice-select.css"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <link
+          rel="stylesheet"
+          href="/assets/css/owl.carousel.min.css"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <link
+          rel="stylesheet"
+          href="/assets/css/slick.css"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <link
+          rel="stylesheet"
+          href="/assets/css/swiper-bundle.min.css"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <link
+          rel="stylesheet"
+          href="/assets/css/seoq-unit.css"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <link
+          rel="stylesheet"
+          href="/assets/css/seoq-updater.css"
+          media="print"
+          onLoad="this.media='all'"
+        />
 
-<script src="/assets/js/main.js"></script>
+        {/* Fallback para navegadores sem JS */}
+        <noscript>
+          <link rel="stylesheet" href="/assets/css/style.css" />
+          <link rel="stylesheet" href="/assets/css/animate.min.css" />
+          <link rel="stylesheet" href="/assets/css/fontawesome-all.min.css" />
+          <link rel="stylesheet" href="/assets/css/magnific-popup.css" />
+          <link rel="stylesheet" href="/assets/css/nice-select.css" />
+          <link rel="stylesheet" href="/assets/css/owl.carousel.min.css" />
+          <link rel="stylesheet" href="/assets/css/slick.css" />
+          <link rel="stylesheet" href="/assets/css/swiper-bundle.min.css" />
+          <link rel="stylesheet" href="/assets/css/seoq-unit.css" />
+          <link rel="stylesheet" href="/assets/css/seoq-updater.css" />
+        </noscript>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
 
-            </body>
-        </Html>
-    )
+        {/* Scripts JS */}
+        <script src="/assets/js/jquery-3.6.0.min.js"></script>
+        <script src="/assets/js/bootstrap.bundle.min.js"></script>
+        <script src="/assets/js/imagesloaded.pkgd.min.js"></script>
+        <script src="/assets/js/isotope.pkgd.min.js"></script>
+        <script src="/assets/js/jquery.appear.min.js"></script>
+        <script src="/assets/js/jquery.countup.min.js"></script>
+        <script src="/assets/js/jquery.magnific-popup.min.js"></script>
+        <script src="/assets/js/jquery.waypoints.min.js"></script>
+        <script src="/assets/js/nice-select.js"></script>
+        <script src="/assets/js/owl.carousel.min.js"></script>
+        <script src="/assets/js/slick.min.js"></script>
+        <script src="/assets/js/swiper-bundle.min.js"></script>
+        <script src="/assets/js/wow.min.js"></script>
+        <script src="/assets/js/main.js"></script>
+      </body>
+    </Html>
+  );
 }
